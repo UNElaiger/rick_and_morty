fcdc430ba8e12090628662e9623a7af726816984
UNElaiger <patrooldiman@mail.ru>
2022-06-30 18:22:55 +0300

    Есть ошибка в Search_bloc.dart

---
+++ lib/feature/presentation/block/search_bloc/search_bloc.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/feature/domain/usecases/serch_person.dart';
+import 'package:rick_and_morty/feature/presentation/block/search_bloc/search_event.dart';
+import 'package:rick_and_morty/feature/presentation/block/search_bloc/search_state.dart';
+
+import '../../../../core/error/failure.dart';
+
+// class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState>{
+//   final SearchPerson searchPerson;
+//   PersonSearchBloc({required this.searchPerson}) : super(PersonEmpty());
+
+//   @override
+//   Stream <PersonSearchState> mapEventToState(PersonSearchEvent event) async*{
+//     if(event is SearchPerson){
+//       yield* _mapFetchPersonsToState(event.personQuery);
+//     }
+//     throw UnimplementedError();
+//   }
+
+//   Stream<PersonSearchState?> _mapFetchPersonsToState(String personQuery) async*{
+//     yield PersonSearchLoading();
+
+//     final failureOrPerson = await searchPerson(SearchPesonParams(query: personQuery));
+
+//     yield failureOrPerson.fold(
+//         (failure) => PersonSearchError(message: _mapFailureToMessage(failure)),
+//         (person) => PersonSearchLoaded(persons: person));
+//   }
+//   }
+
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
+
+class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
+  final SearchPersons searchPerson;
+
+  PersonSearchBloc({required this.searchPerson}) : super(PersonSearchEmpty());
+
+  @override
+  Stream<PersonSearchState> mapEventToState(PersonSearchEvent event) async* {
+    if (event is SearchPersons) {
+      yield* _mapFetchPersonsToState(event.personQuery);
+    }
+  }
+
+  Stream<PersonSearchState> _mapFetchPersonsToState(String personQuery) async* {
+    yield PersonSearchLoading();
+
+    final failureOrPerson =
+        await searchPerson(SearchPesonParams(query: personQuery));
+
+    yield failureOrPerson.fold(
+        (failure) => PersonSearchError(message: _mapFailureToMessage(failure)),
+        (person) => PersonSearchLoaded(persons: person));
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHED_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
+  }
+}
---
+++ lib/feature/presentation/block/search_bloc/search_event.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+// abstract class PersonSearchEvent extends Equatable {
+//   const PersonSearchEvent();
+//   @override
+//   List<Object> get props => [];
+// }
+
+// class SearchPersons extends PersonSearchEvent{
+//   final String personQuery;
+
+//   SearchPersons (this.personQuery);
+// }
+
+abstract class PersonSearchEvent extends Equatable {
+  const PersonSearchEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class SearchPersons extends PersonSearchEvent {
+  final String personQuery;
+
+  SearchPersons(this.personQuery);
+
+}
